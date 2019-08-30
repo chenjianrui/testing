ddbf56fd3c2887e65a82f75a120b7e3fdff4d404
chenjianrui <sky75217@gmail.com>
2019-08-30 17:06:34 +0800

    把各別的測試分開寫

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -8,5 +8,10 @@ it('show a comment box', () => {
   const wrapped = shallow(<App />);
 
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('show a comment list', () => {
+  const wrapped = shallow(<App />);
+
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
