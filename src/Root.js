a3d03b8aa68a171acb60bcbe70903ece3c0aa6fb
chenjianrui <sky75217@gmail.com>
2019-09-03 14:52:09 +0800

    將 Root 新增初始化，讓 CommentList 可以測試，因為 CommentList 要有預設的 list ，不然無法跑測試

--- src/Root.js
+++ src/Root.js
@@ -3,8 +3,8 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from 'reducers';
 
-const store = createStore(reducers);
-
-export default props => {
-  return <Provider store={store}>{props.children}</Provider>;
+export default ({ children, initialState = {} }) => {
+  return (
+    <Provider store={createStore(reducers, initialState)}>{children}</Provider>
+  );
 };
